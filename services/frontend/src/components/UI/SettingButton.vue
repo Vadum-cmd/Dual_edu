d5acc5e1753e565d0001ccf59425527059094fc3
OKmeray <yar.rom.rom@gmail.com>
2023-02-19 20:45:16 +0200

    Reuploaded frontend

---
+++ services/frontend/src/components/UI/SettingButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'setting-button'
+}
+</script>
+
+<style scoped>
+.btn {
+  height: 45px;
+  width: 120px;
+  background-color: #716eff;
+  color: #ffffff;
+  border-radius: 40px;
+  border: none;
+}
+</style>
