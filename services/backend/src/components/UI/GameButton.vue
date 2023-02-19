bb299b9cc87a58ceaab9ef3e44bf456370c54f27
OKmeray <yar.rom.rom@gmail.com>
2023-02-19 19:17:26 +0200

    Reuploaded backend

---
+++ services/backend/src/components/UI/GameButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'game-button'
+}
+</script>
+
+<style scoped>
+.btn {
+  height: 85px;
+  width: 220px;
+  background-color: #d9d9d9;
+  color: #000000;
+  border-radius: 20px;
+  border: none;
+  transition:0.5s ease-out;
+}
+.btn:hover{
+  background-color: #807c7c;
+}
+</style>
