380ff3a6760eaaa1f9d10092ea81dddbca236c4c
OKmeray <yar.rom.rom@gmail.com>
2023-02-26 18:04:02 +0200

    Fixed some endpoints

--- services/backend/routers/vocabulary.py
+++ services/backend/routers/vocabulary.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from dependencies import get_db
-from crud.crud import get_user, get_user_words
+from crud.crud_functions import get_user_vocab
 
 app = FastAPI()
 
 @app.get("/vocabulary")
 def get_vocabulary(user_id: int, db: Session = Depends(get_db)):
-    user = get_user(db=db, user_id=user_id)
-    user_vocabulary = get_user_words(db=db, user_id=user_id)
-    return {"user_name": user.user_name, "user_level": user.user_level}  # TODO: add user_vocabulary
-
+    user_vocabulary = get_user_vocab(db=db, user_id=user_id)
+    return user_vocabulary
--- services/backend/routers/words_test.py
+++ services/backend/routers/words_test.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from dependencies import get_db
-from crud.crud import get_word, update_word
-from schemas.new_schema import WordUpdate
+from crud.crud_functions import get_user_word, get_word
+# from schemas.new_schema import WordUpdate
 
 app = FastAPI()
 
 @app.get("/test")
 def get_random_word(user_id: int, db: Session = Depends(get_db)):
-    word = get_random_word(db=db, user_id=user_id)
-    return {"word_id": word.id, "word": word.word}
+    word = get_user_word(db=db, user_id=user_id)
+    return {"word_id": word.word_id, "word": word.en_word}
 
 @app.post("/test")
-def check_word(user_id: int, word_id: int, word: str, db: Session = Depends(get_db)):
-    if word.lower() == get_word(db=db, word_id=word_id).lower():
-        update_word(db=db, word_id=word_id, word=WordUpdate())  # TODO: WordUpdate is not written yet
+def check_word(word_id: int, answer: str, db: Session = Depends(get_db)):
+    if answer.lower() == get_word(db=db, word_id=word_id).uk_word.lower():
+        #update_word(db=db, word_id=word_id, word=WordUpdate())
+        # TODO: write update_word
         return {"result": True}
     return {"result": False, "correct_answer": get_word(db=db, word_id=word_id).uk_word}
 
