5e6b1b91ccd84d810d3ed784e2219a2030be473d
Rednigo <barostikq@gmail.com>
2023-03-02 15:43:28 +0200

    Made parser

--- services/backend/html_parser/parser.py
+++ services/backend/html_parser/parser.py
@@ -1,30 +1,12 @@
-from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
-from selenium.webdriver.support.ui import Select
-import time
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait as wait
 import csv
 
-# Using Chrome to access web
-url='https://www.englishprofile.org/american-english'
-driver = webdriver.Chrome()
-# Open the website
-driver.get(url)
+# Make request 
+html=requests.get("https://www.englishprofile.org/wordlists/evp?filter_search=&filter_custom_Level%5B%5D=1&filter_custom_Level%5B%5D=2&filter_custom_Level%5B%5D=3&filter_custom_Level%5B%5D=4&filter_custom_Level%5B%5D=5&filter_custom_Level%5B%5D=6&filter_custom_Topic=&filter_custom_Parts=&filter_custom_Category=1&filter_custom_Grammar=&filter_custom_Usage=&filter_custom_Prefix=&filter_custom_Suffix=&limit=0&directionTable=asc&sortTable=base&task=&boxchecked=0&filter_order=pos_rank&filter_order_Dir=asc&c536d88c2638e9654405bcaa8620fed2=1")
 
-#driver.find_element('id','filter_custom_Level2').click()
-wait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="adminForm"]/div[1]/div[2]/div/fieldset/div/label[3]'))).click()
-button = driver.find_element('xpath','//*[@id="adminForm"]/div[1]/div[3]/button')
-button.click()
-drop_down = Select(driver.find_element('id','limit'))
-drop_down.select_by_value('0')
-
-html_content = requests.get(driver.current_url).text
- 
 # Parsing the HTML
-soup = BeautifulSoup(html_content, 'html.parser')
+soup = BeautifulSoup(html.content, 'html.parser')
 # Find the table element by its ID
 table = soup.find('table', {'id':'reportList'})
 # Open a new CSV file for writing
@@ -34,13 +16,19 @@ with open('data.csv', 'w', newline='') as f:
 
     # Find all the rows in the table
     rows = tbody.find_all('tr')
-    
-    # Loop through the rows and extract the first cell
+    # Write column names
+    writer.writerow(["en_word", "word_level", "uk_word"])
+    # Create word list to avoid duplicates of words
+    word_list=[]
+    # Loop through the rows and extract the cells
     for row in rows:
         cells = row.find_all('td')
-        first_cell = cells[0].text.strip()
-
+        word_cell = cells[0].text.strip()
+        level_cell = cells[2].text.strip()       
         # Write the data to the CSV file
-        writer.writerow([first_cell])
+        if word_cell not in word_list:
+            word_list.append(word_cell)
+            writer.writerow([word_cell,level_cell])
     
+        
         
