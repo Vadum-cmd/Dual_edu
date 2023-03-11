858638695c3e1ec8f98c27198704b9ff6768aacd
OKmeray <yar.rom.rom@gmail.com>
2023-03-11 19:18:55 +0200

    Removed some files from .gitignore

--- services/backend/routers/download_file.py
+++ services/backend/routers/download_file.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-from main import app
+from typing import List
 
+from fastapi import Depends, APIRouter
+from fastapi.responses import FileResponse
+from sqlalchemy.orm import Session
+from dependencies import get_db
+from crud.crud_functions import get_db_word_by_en_word, change_user_word_status, get_random_user_book, \
+    get_unknown_user_word_by_book_id
 
 router = APIRouter()
 
-@router.get("/download-vocabulary", response_class=FileResponse)
-async def main():
-    return #some_file_path
+@router.get("/vocabulary/download/", response_class=FileResponse)
+def download_table(user_id: int, levels: List[str], db: Session = Depends(get_db)):
+    return None
