5320ebcc3f396b70492ff9375872f5bd2160c04b
Vadum-cmd <vadum.treskot@gmail.com>
2023-03-26 23:11:48 +0300

    modified word tokenization, added phrasal verbs search

--- services/backend/logic/extract_words_and_translate.py
+++ services/backend/logic/extract_words_and_translate.py
@@ -1,8 +1,14 @@
 import PyPDF2
+import nltk
 from nltk.tokenize import RegexpTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
 from translators import translate_text
 
-def extract_text_from_pdf(file_path):
+
+nltk.download('wordnet')
+test_phrasal = ["ask out", "blow up", "back up"]
+
+def extract_text_from_pdf(file_path: str):
     with open(file_path, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         text = ''
@@ -11,11 +17,24 @@ def extract_text_from_pdf(file_path):
             text += page.extract_text()
         return text
 
-def word_tokenization(text):
+def word_tokenization(text: str):
     tokenizer = RegexpTokenizer(r'\w+')
     words = tokenizer.tokenize(text)
-    words = set([word.lower() for word in words if word.isalpha()])
+    words = [word.lower() for word in words if word.isalpha()]
+    for i in range(len(words)):
+        words[i] = WordNetLemmatizer().lemmatize(words[i], 'v')
     return words
+
+def search_phrasal(lst: List):
+    phrasal = set()
+    for i in range(len(lst) - 2):
+        together = lst[i] + " " + lst[i+1]
+        separated = lst[i] + " " + lst[i+2]
+        if together in test_phrasal:
+            phrasal.add(together)
+        if separated in test_phrasal:
+            phrasal.add(separated)
+    return phrasal
 
 def translate_word(word):
     return translate_text(query_text=word, translator="google", from_language="en", to_language="uk")
