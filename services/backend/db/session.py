a7a1ba050df1c22360add07f286bdf432e33d008
OKmeray <94756206+OKmeray@users.noreply.github.com>
2023-04-08 08:40:27 +0300

    Deleted some lines to resolve merge conflict

--- services/backend/db/session.py
+++ services/backend/db/session.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = "mysql://sql7603430:HZalYsm789@sql7.freemysqlhosting.net:3306/sql7603430"
-SQLALCHEMY_DATABASE_URL = "mysql://root:2wsxcde3@127.0.0.1:3307/dual_edu"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
     
