3a3400bc520fb4264fd0dadbf6fb55395c2f5eb2
Javenhun733r <andrij.babyuk@gmail.com>
2023-04-26 09:54:05 +0300

    Fixed bugs and upgrade site func

--- services/frontend/src/components/pages/Verification.vue
+++ services/frontend/src/components/pages/Verification.vue
@@ -1,21 +1,26 @@
 <template>
-<div>
+<div v-if="isNotVerificated">
+
   <h1>Click to verify your email</h1>
   <setting-button @click="sendToken">Verify</setting-button>
 </div>
-
+<div v-else>
+  <h1>You can finally login</h1>
+  <setting-button @click="loginRedirect">Login</setting-button>
+</div>
 </template>
 
 <script>
 import axios from "axios";
 import SettingButton from "@/components/UI/SettingButton.vue";
+import router from "@/components/router/index.js";
 export default {
   name: "Verifi-cation",
   components: {SettingButton},
   data() {
     return {
 
-
+      isNotVerificated:true,
       token:'',
       url:process.env.VUE_APP_URL
     }
@@ -25,8 +30,15 @@ export default {
       const data = {
         token: this.token,
       }
-      axios.post(this.url + '/verify', data)
+      const response = axios.post(this.url + '/verify', data)
+      if(response.ok){
+        this.isNotVerificated=false;
+      }
+    },
+    loginRedirect(){
+      router.push('/login');
     }
+
   },
 
   mounted() {
@@ -39,5 +51,7 @@ export default {
 </script>
 
 <style scoped>
-
+div{
+  color:white;
+}
 </style>
